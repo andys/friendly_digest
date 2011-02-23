087a2fbdd710b174f598dc954235234d12cbf37d
Andrew Snow <andrew@modulus.org>
2011-02-23 15:32:21 +1100

    v1.1. Flag which adds a one character checksum for sanity checking

--- lib/friendly_digest.rb
+++ lib/friendly_digest.rb
@@ -1,5 +1,6 @@
 
 class FriendlyDigest < String
+  class InvalidInputException < Exception ; end
   class << self
     def [](*p) ; new(*p) ; end
     
@@ -7,18 +8,33 @@ class FriendlyDigest < String
       @codes ||= ['Z','2','B','T','C','9','H','Q','V','K','N','W','7','P','4','J','Y','G','E','A','D','3','F','8','M','X','6','U','R']
     end
     
-    def decode(input)
-      input.strip.upcase.split('').inject(0) {|tot,c| (tot * codes.length) + codes.index(c) }
+    def numcodes
+      @numcodes ||= codes.length
+    end
+    
+    def decode(input, parity=false)
+      cleaned = input.strip.upcase.split('')
+      raise InvalidInputException.new "Invalid character" if cleaned.any?(&:nil?)
+      raise InvalidInputException.new "Invalid input" if parity && cleaned.pop!= checksum(cleaned)
+      
+      cleaned.map {|c| codes.index(c) }.inject(0) {|tot,c| (tot * numcodes) + c }
+    end
+    
+    def checksum(array)
+      codes[array.inject(0) {|sum,c| (sum + codes.index(c)) % numcodes }]
     end
   end
 
-  def initialize(input)
+  def initialize(input, parity=false)
     output = input == 0 ? [self.class.codes.first] : []
     while input > 0
       rem = input % self.class.codes.length
-      input = (input - rem) / self.class.codes.length #/
+      input = (input - rem) / self.class.numcodes #/
       output << self.class.codes[rem]
     end
     output.reverse.each {|c| self << c }
+    self << self.class.checksum(output) if parity
   end
+  
+  
 end
